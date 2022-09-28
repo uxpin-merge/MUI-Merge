f5316d643a2bc862920fd2aaaf949df3e1d66773
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2022-09-28 13:40:36 -0600

    Card updates

--- src/components/Card/presets/0-default.jsx
+++ src/components/Card/presets/0-default.jsx
@@ -12,7 +12,7 @@ import IconButton from '../../IconButton/IconButton';
 import Typography from '../../Typography/Typography';
 
 export default (
-  <Card uxpId='Card-1' sx={{ maxWidth: 345 }}>
+  <Card uxpId='Card-1'>
     <CardHeader
       uxpId='Card-Header-1'
       avatar="R"
@@ -28,8 +28,7 @@ export default (
       image="https://images.unsplash.com/photo-1604871000636-074fa5117945?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
       alt="Cool art" 
     />
-    <CardActionArea uxpId='Card-Action-Area-1'>
-      <CardContent uxpId='Card-Content-1' component='div'>
+     <CardContent uxpId='Card-Content-1' component='div'>
         <Typography uxpId="Typograhpy-1" gutterBottom variant="h4" component='div'>
           Trends
         </Typography>
@@ -37,11 +36,12 @@ export default (
           Learn about the recent top design trends and get inspired. See what you can experiment with in your future designs.
         </Typography>
       </CardContent>
-    </CardActionArea>
+    <CardActionArea uxpId='Card-Action-Area-1'>
     <CardActions uxpId='Card-Action-1'>
-      <Button uxpId='Button-1' size="small" color="primary">
+      <Button uxpId='Button-1' size="small" color="primary" variant="outlined">
         Share
       </Button>
     </CardActions>
+    </CardActionArea>
   </Card>
 );
