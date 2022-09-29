883a75a0b243a68ac25c455092ad19350fe1f97c
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2022-09-28 18:39:54 -0600

    added scoped css base

--- src/components/UXPinWrapper/UXPinWrapper.js
+++ src/components/UXPinWrapper/UXPinWrapper.js
@@ -3,10 +3,13 @@ import React from "react";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import boilerplateTheme from "./boilerplate-theme";
 import Paper from "@mui/material/Paper";
+import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
 
 const theme = createTheme(boilerplateTheme);
 
 export default function UXPinWrapper({ children }) {
 
-  return (<ThemeProvider theme={theme}><Paper elevation={0} style={{backgroundColor: "transparent"}}>{children}</Paper></ThemeProvider>);
+  return (<ThemeProvider theme={theme}>    <ScopedCssBaseline>
+    <Paper elevation={0} style={{backgroundColor: "transparent"}}>{children}</Paper>    </ScopedCssBaseline>
+</ThemeProvider>);
 } 
