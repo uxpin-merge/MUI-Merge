034a1e3b55880f72ede9f2d21540618638c7f29f
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2022-09-29 12:05:51 -0600

    default color

--- src/components/Dot/presets/0-default.jsx
+++ src/components/Dot/presets/0-default.jsx
@@ -3,7 +3,7 @@ import Dot from "../Dot"
 import Typography from '../../Typography/Typography';
 
 export default (
-  <Dot uxpId="dot-1" direction="row" color="warning" size={10} spacing={2}>
+  <Dot uxpId="dot-1" direction="row" color="warning.main" size={10} spacing={2}>
     <Typography variant="body1" uxpId="dot-typography-1">
       Warning
     </Typography>
