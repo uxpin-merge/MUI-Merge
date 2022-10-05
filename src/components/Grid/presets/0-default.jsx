390cb6895e1691722fcf37e6538caa5cdf1a0001
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2022-10-05 11:15:25 -0600

    Clean up

--- src/components/Grid/presets/0-default.jsx
+++ src/components/Grid/presets/0-default.jsx
@@ -4,64 +4,24 @@ import Button from "../../Button/Button";
 
 export default (
   <Grid container spacing="8" uxpId="1">
-    <Grid item xs={3} order={1} uxpId="1.1">
+    <Grid item xs={4} order={1} uxpId="1.1">
       <Button
         uxpId="1.1.1"
         size="medium"
         variant="outlined"
         color="primary"
         fullWidth>
-        3 cols
+        4 cols
       </Button>
     </Grid>
-    <Grid item xs={3} order={2}  uxpId="1.2">
+    <Grid item xs={8} order={2}  uxpId="1.2">
       <Button
         uxpId="1.2.1"
         size="medium"
         variant="outlined"
         color="primary"
         fullWidth>
-        3 cols
-      </Button>
-    </Grid>
-    <Grid item xs={3} order={3} uxpId="1.3">
-      <Button
-        uxpId="1.3.1"
-        size="medium"
-        variant="outlined"
-        color="primary"
-        fullWidth>
-        3 cols
-      </Button>
-    </Grid>
-    <Grid item xs={3} order={4} uxpId="1.4">
-      <Button
-        uxpId="1.4.1"
-        size="medium"
-        variant="outlined"
-        color="primary"
-        fullWidth>
-        3 cols
-      </Button>
-    </Grid>
-    <Grid item xs={8} order={5}  uxpId="1.5">
-      <Button
-        uxpId="1.5.1"
-        size="medium"
-        variant="outlined"
-        color="primary"
-        fullWidth>
         8 cols
-      </Button>
-    </Grid>
-    <Grid item xs={4} order={6} uxpId="1.6">
-      <Button
-        uxpId="1.6.1"
-        size="medium"
-        variant="outlined"
-        color="primary"
-        fullWidth>
-        4 cols
       </Button>
     </Grid>
   </Grid>
