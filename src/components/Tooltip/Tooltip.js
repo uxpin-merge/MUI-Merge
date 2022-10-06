0bbc52f354e0247083bbf877e792140589d62885
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2022-10-05 18:06:19 -0600

    Fixed Tooltip

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import PropTypes from "prop-types";
 import TooltipM from '@mui/material/Tooltip';
 
@@ -7,8 +7,8 @@ import TooltipM from '@mui/material/Tooltip';
  */
 function Tooltip(props) {
   return (
-      <TooltipM {...props} PopperProps={{ keepMounted: true, disablePortal: true }}>
-        <div>{props.children}</div>
+      <TooltipM {...props}>
+        <div >{props.children}</div>
       </TooltipM>
   );
 }
