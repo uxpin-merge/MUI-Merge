9e2579fb4f4dc8150942cdd38b80fd61120c6072
Evan Trujillo <evantrujillo30@gmail.com>
2023-01-31 14:36:12 +0900

    Add uxpin ref and data grid props

--- src/components/DataGrid/presets/0-default.jsx
+++ src/components/DataGrid/presets/0-default.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import DataGrid from '../DataGrid';
 
 const rows = [
-    { id: 1, col1: 'Hello', col2: 'World' },
-    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-    { id: 3, col1: 'MUI', col2: 'is Amazing' },
+    { id: 1, col1: 'Hello', col2: 'World', col3: 'is here' },
+    { id: 2, col1: 'DataGrid', col2: 'is Awesome', col3: 'with UXPin' },
+    { id: 3, col1: 'MUI', col2: 'is Amazing', col3: 'with UXPin' },
 ];
 
 const columns = [
     { field: 'col1', headerName: 'Column 1', width: 150 },
     { field: 'col2', headerName: 'Column 2', width: 150 },
+    { field: 'col3', headerName: 'Column 3', width: 150 },
+
 ];
 
 export default (
@@ -17,5 +19,6 @@ export default (
         uxpId='data-grid'
         columns={columns}
         rows={rows}
+        sx={{ height: '350px', width: '500px'}}
         />
 );
