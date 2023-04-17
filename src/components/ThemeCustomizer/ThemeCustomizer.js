8da2aee34f3ed2c00c9af79395c9f022cd77e2d3
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-04-17 12:04:04 -0600

    adding iframe

--- src/components/ThemeCustomizer/ThemeCustomizer.js
+++ src/components/ThemeCustomizer/ThemeCustomizer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { createTheme } from '@mui/material/styles';
 import { ThemeContext } from '../UXPinWrapper/UXPinWrapperWithThemeCustomizer';
-
+import { Portal } from '@mui/base';
 //Will add custom font links to the header
 const addFont = (link, index) => {
   let newFontLink = document.createElement('link');
@@ -70,11 +70,13 @@ function ThemeCustomizer(props) {
   }, [props, setThemeOptions, themeOptions.themeCustomizerProps]); //only re-run if any of these change
 
   return (
-    <div>
-      <h1>Global Theme Customizer</h1>
-      <p>Should change global theme props</p>
-      <div style={{ marginTop: '2em' }}>{props.children}</div>
-    </div>
+    <Portal container={document.querySelector('.ng-canvas')}>
+      <iframe width={300} height={300}>
+        <h1>Global Theme Customizer</h1>
+        <p>Should change global theme props</p>
+        <a href="http://www.google.com">test</a>{' '}
+      </iframe>
+    </Portal>
   );
 }
 
