39e824fa879c4dd0782b8699a99b030b22f191f7
Evan Trujillo <evantrujillo30@gmail.com>
2023-08-17 14:24:57 +0900

    Update Avatar
    * Update AvatarGroup

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AvatarM, { AvatarProps } from '@mui/material/Avatar';
+
+/**
+ * @uxpindocurl https://mui.com/api/avatar/
+ */
+function Avatar(props: AvatarProps) {
+  return <AvatarM {...props} />;
+}
+
+
+
+export default Avatar;
---
+++ src/components/AvatarGroup/AvatarGroup.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import AvatarGroupM, { AvatarGroupProps } from '@mui/material/AvatarGroup';
+
+/**
+ * @uxpindocurl https://mui.com/api/avatar-group/#main-content
+ */
+function AvatarGroup(props: AvatarGroupProps) {
+  return <AvatarGroupM {...props} />
+}
+
+export default AvatarGroup;
