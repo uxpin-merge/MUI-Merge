f97fadd9d4e2b7489005a38cf3aaf7f3a9ce0ded
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-10-16 11:28:19 -0600

    cleanup

--- src/components/AccordionActions/AccordionActions.js
+++ src/components/AccordionActions/AccordionActions.js
@@ -12,20 +12,18 @@ function AccordionActions(props) {
 AccordionActions.propTypes = {
   /**
    * The content of the component.
-   * @uxpinignoreprop
    */
   children: PropTypes.node,
 
   /**
    * Override or extend the styles applied to the component.
-   * @uxpinignoreprop
    */
   classes: PropTypes.object,
 
   /**
    * If true, the actions do not have additional margin.
    */
-  disableSpacing: PropTypes.bool.prototype,
+  disableSpacing: PropTypes.bool,
 
   /**
    * The system prop that allows defining system overrides as well as additional CSS styles.
