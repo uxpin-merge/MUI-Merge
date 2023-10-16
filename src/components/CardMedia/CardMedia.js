37704c006f4358d11f7f5af81ba8f88e89ade3f2
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-10-16 12:33:16 -0600

    cleanup

--- src/components/CardMedia/CardMedia.js
+++ src/components/CardMedia/CardMedia.js
@@ -45,11 +45,5 @@ CardMedia.propTypes = {
   sx: PropTypes.object,
 };
 
-CardMedia.defaultProps = {
-  component: 'img',
-  height: 120,
-  image:
-    'https://images.unsplash.com/photo-1604871000636-074fa5117945?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-};
 
 export { CardMedia as default };
