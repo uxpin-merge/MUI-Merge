d6e25a7bd9fb22c806bf3610c3407093696c2e89
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-10-19 10:09:33 -0600

    cleanup

--- src/components/Dialog/presets/0-default.jsx
+++ src/components/Dialog/presets/0-default.jsx
@@ -5,12 +5,19 @@ import DialogActions from '../../DialogActions/DialogActions';
 import DialogContent from '../../DialogContent/DialogContent';
 import DialogContentText from '../../DialogContentText/DialogContentText';
 import DialogTitle from '../../DialogTitle/DialogTitle';
+import IconButton from '../../IconButton/IconButton';
+import Icon from '../../Icon/Icon';
+import Stack from '../../Stack/Stack';
 
 export default (
   <Dialog uxpId="3" aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description" open={true}>
-    <DialogTitle id="alert-dialog-title" close={true} uxpId="4">
+    <Stack direction="row" alignItems="center" justifyContent="space-between" uxpId="stack"><DialogTitle id="alert-dialog-title" sx={{}} uxpId="4">
       {"Use Google's location service?"}
     </DialogTitle>
+      <IconButton uxpId="Icon-Button-1" sx={{ "marginRight": "16px" }}>
+        <Icon uxpId="icon">close</Icon>
+      </IconButton>
+    </Stack>
     <DialogContent uxpId="5">
       <DialogContentText id="alert-dialog-description" uxpId="6">
         Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps
