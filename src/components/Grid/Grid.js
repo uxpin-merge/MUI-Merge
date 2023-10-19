0994966f22799cdd975f0bd1c8f40549375f7fb6
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-10-19 11:09:01 -0600

    cleanup

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -4,20 +4,12 @@ import GridM from '@mui/material/Grid';
 
 /**
  * @uxpindocurl https://mui.com/api/grid/
+ * @uxpindescription The Material Design responsive layout grid adapts to screen size and orientation, ensuring consistency across layouts.
  */
 function Grid(props) {
   // const space = Number(props.spacing);
   return (
-    <GridM
-      {...props}
-      spacing={props.spacing}
-      sx={{
-        paddingTop: props.paddingTop + 'px',
-        paddingBottom: props.paddingBottom + 'px',
-        paddingRight: props.paddingRight + 'px',
-        paddingLeft: props.paddingLeft + 'px',
-      }}
-    >
+    <GridM{...props}>
       {props.children}
     </GridM>
   );
@@ -25,43 +17,9 @@ function Grid(props) {
 
 Grid.propTypes = {
   /**
-   * Defines the `align-content` style property.
-   * It's applied for all screen sizes.
-   */
-  alignContent: PropTypes.oneOf(['stretch', 'center', 'flex-start', 'flex-end', 'space-between', 'space-around']),
-
-  /**
-   * Defines the `align-items` style property.
-   * It's applied for all screen sizes.
-   */
-  alignItems: PropTypes.oneOf(['flex-start', 'center', 'flex-end', 'stretch', 'baseline']),
-
-  /**
-   * @uxpinignoreprop
-   * The content of the component.
-   */
+ * The content of the component.
+ */
   children: PropTypes.node,
-
-  /**
-   * @uxpinignoreprop
-   * Override or extend the styles applied to the component.
-   * See [CSS API](#css-api) below for more details.
-   */
-  classes: PropTypes.object,
-
-  /**
-   * @uxpinignoreprop
-   * @ignore
-   */
-  className: PropTypes.string,
-
-  /**
-   * @uxpinignoreprop
-   * The component used for the root node.
-   * Either a string to use a DOM element or a component.
-   */
-  component: PropTypes.string,
-
   /**
    * If `true`, the component will have the flex *container* behavior.
    * You should be wrapping *items* with a *container*.
@@ -69,17 +27,65 @@ Grid.propTypes = {
   container: PropTypes.bool,
 
   /**
-   * Defines the `flex-direction` style property.
-   * It is applied for all screen sizes.
-   */
-  direction: PropTypes.oneOf(['row', 'row-reverse', 'column', 'column-reverse']),
-
-  order: PropTypes.number,
-  /**
-   * If `true`, the component will have the flex *item* behavior.
-   * You should be wrapping *items* with a *container*.
-   */
+ * If `true`, the component will have the flex *item* behavior.
+ * You should be wrapping *items* with a *container*.
+ */
   item: PropTypes.bool,
+  /**
+ * Defines the `flex-direction` style property.
+ * It is applied for all screen sizes.
+ */
+  direction: PropTypes.oneOf(['row', 'row-reverse', 'column', 'column-reverse']),
+  /**
+* The number of columns. Usually 12
+*/
+  columns: PropTypes.number,
+  /**
+  * Defines the space between the type `item` component.
+  * It can only be used on a type `container` component.
+  */
+  spacing: PropTypes.string,
+
+  /**
+* Defines the horizontal space between the type item components. It overrides the value of the spacing prop.
+*/
+  columnSpacing: PropTypes.number,
+  /**
+* Defines the vertical space between the type item components. It overrides the value of the spacing prop.
+*/
+  rowSpacing: PropTypes.number,
+
+  /**
+ * Defines the `flex-wrap` style property.
+ * It's applied for all screen sizes.
+ */
+  wrap: PropTypes.oneOf(['nowrap', 'wrap', 'wrap-reverse']),
+
+
+  /**
+  * Override or extend the styles applied to the component.
+  * See [CSS API](#css-api) below for more details.
+  */
+  classes: PropTypes.object,
+
+
+  /**
+  * Defines the horizontal space between the type item components. It overrides the value of the spacing prop.
+  */
+  component: PropTypes.string,
+
+  /**
+   * Defines the `align-content` style property.
+   * It's applied for all screen sizes.
+   */
+  alignContent: PropTypes.oneOf(['stretch', 'center', 'flex-start', 'flex-end', 'space-between', 'space-around']),
+
+  /**
+   * The component used for the root node.
+   * Either a string to use a DOM element or a component.
+   */
+  component: PropTypes.string,
+
 
   /**
    * Defines the `justify-content` style property.
@@ -95,59 +101,58 @@ Grid.propTypes = {
   ]),
 
   /**
-   * Defines the number of grids the component is going to use.
-   * It's applied for the `lg` breakpoint and wider screens if not overridden.  1-12, true, false, or auto.
-   */
-  lg: PropTypes.oneOfType([PropTypes.number, PropTypes.bool, PropTypes.string]),
-
-  /**
-   * Defines the number of grids the component is going to use.
-   * It's applied for the `md` breakpoint and wider screens if not overridden.  1-12, true, false, or auto.
-   */
-  md: PropTypes.oneOfType([PropTypes.number, PropTypes.bool, PropTypes.string]),
-
-  /**
-   * Defines the number of grids the component is going to use.
-   * It's applied for the `sm` breakpoint and wider screens if not overridden.  1-12, true, false, or auto.
-   */
-  sm: PropTypes.oneOfType([PropTypes.number, PropTypes.bool, PropTypes.string]),
-
-  /**
-   * Defines the space between the type `item` component.
-   * It can only be used on a type `container` component.
-   */
-  spacing: PropTypes.oneOf(['0', '8', '16', '24', '32', '40']),
-
-  /**
-   * Defines the `flex-wrap` style property.
-   * It's applied for all screen sizes.
-   */
-  wrap: PropTypes.oneOf(['nowrap', 'wrap', 'wrap-reverse']),
-
-  /**
-   * Defines the number of grids the component is going to use.
-   * It's applied for the `columns (xl)` breakpoint and wider screens.  1-12, true, false, or auto.
-   * @uxpinpropname  columns (xl)
-   */
-  xl: PropTypes.oneOfType([PropTypes.number, PropTypes.bool, PropTypes.string]),
+ * Defines the `align-items` style property.
+ * It's applied for all screen sizes.
+ */
+  alignItems: PropTypes.oneOf(['flex-start', 'center', 'flex-end', 'stretch', 'baseline']),
 
   /**
    * Defines the number of grids the component is going to use.
    * It's applied for all the screen sizes with the lowest priority. 1-12, true, false, or auto.
-   * @uxpinpropname  columns (xs)
    */
   xs: PropTypes.oneOfType([PropTypes.number, PropTypes.bool, PropTypes.string]),
+  /**
+ * Defines the number of grids the component is going to use.
+ * It's applied for the `sm` breakpoint and wider screens if not overridden.  1-12, true, false, or auto.
+ */
+  sm: PropTypes.oneOfType([PropTypes.number, PropTypes.bool, PropTypes.string]),
+  /**
+* Defines the number of grids the component is going to use.
+* It's applied for the `md` breakpoint and wider screens if not overridden.  1-12, true, false, or auto.
+*/
+  md: PropTypes.oneOfType([PropTypes.number, PropTypes.bool, PropTypes.string]),
+  /**
+ * Defines the number of grids the component is going to use.
+ * It's applied for the `lg` breakpoint and wider screens if not overridden.  1-12, true, false, or auto.
+ */
+  lg: PropTypes.oneOfType([PropTypes.number, PropTypes.bool, PropTypes.string]),
 
+  /**
+   * Defines the number of grids the component is going to use.
+   * It's applied for the `columns (xl)` breakpoint and wider screens.  1-12, true, false, or auto.
+   */
+  xl: PropTypes.oneOfType([PropTypes.number, PropTypes.bool, PropTypes.string]),
   /**
    * If `true`, it sets `min-width: 0` on the item.
    * Refer to the limitations section of the documentation to better understand the use case.
    */
   zeroMinWidth: PropTypes.bool,
-
-  paddingTop: PropTypes.number,
-  paddingBottom: PropTypes.number,
-  paddingRight: PropTypes.number,
-  paddingLeft: PropTypes.number,
+  /**
+   * Padding Top in px
+   */
+  paddingTop: PropTypes.string,
+  /**
+ * Padding Bottom in px
+ */
+  paddingBottom: PropTypes.string,
+  /**
+ * Padding Right in px
+ */
+  paddingRight: PropTypes.string,
+  /**
+ * Padding Left in px
+ */
+  paddingLeft: PropTypes.string,
 
   /**
    * The system prop that allows defining system overrides as well as additional CSS styles.
--- src/components/Grid/presets/0-default.jsx
+++ src/components/Grid/presets/0-default.jsx
@@ -4,14 +4,14 @@ import Button from '../../Button/Button';
 
 export default (
   <Grid container spacing="8" uxpId="1">
-    <Grid item xs={4} order={1} uxpId="1.1">
+    <Grid item xs={12} sm={6} uxpId="1.1">
       <Button uxpId="1.1.1" size="medium" variant="outlined" color="primary" fullWidth>
-        4 cols
+        Button
       </Button>
     </Grid>
-    <Grid item xs={8} order={2} uxpId="1.2">
+    <Grid item xs={12} sm={6} uxpId="1.2">
       <Button uxpId="1.2.1" size="medium" variant="outlined" color="primary" fullWidth>
-        8 cols
+        Button
       </Button>
     </Grid>
   </Grid>
