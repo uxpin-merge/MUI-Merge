314a359a5744d1a72366f8eb6dc8e77d2940fa23
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-10-24 11:12:38 -0600

    cleanup

--- src/components/IconButton/presets/0-default.jsx
+++ src/components/IconButton/presets/0-default.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import IconButton from '../IconButton';
 import Icon from '../../Icon/Icon';
 
-export default <IconButton uxpId="Icon-Button-1"><Icon uxpId="icon">home</Icon></IconButton>;
+export default <IconButton uxpId="Icon-Button-1"><Icon uxpId="icon" fontSize="inherit">home</Icon></IconButton>;
