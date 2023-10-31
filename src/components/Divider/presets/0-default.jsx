14d7d542a57313660f1df3d17b54e34e62dba626
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-10-31 11:11:58 -0600

    cleanup

--- src/components/Divider/presets/0-default.jsx
+++ src/components/Divider/presets/0-default.jsx
@@ -3,7 +3,7 @@ import Divider from '../Divider';
 import Typography from '../../Typography/Typography';
 
 export default (
-  <Divider uxpId="divider-1">
+  <Divider uxpId="divider-1" variant="fullWidth" flexItem={true} orientation="horizontal">
     <Typography uxpId="divider-child">Optional Child</Typography>
   </Divider>
 );
