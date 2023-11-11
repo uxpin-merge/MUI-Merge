ffd309233026caf2add82224e7f7a1f291236c6f
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-11-11 11:53:28 -0700

    cleanup

--- src/components/Grid/presets/0-default.jsx
+++ src/components/Grid/presets/0-default.jsx
@@ -6,12 +6,12 @@ export default (
   <Grid container spacing="8" uxpId="1">
     <Grid item xs={12} sm={6} uxpId="1.1">
       <Button uxpId="1.1.1" size="medium" variant="outlined" color="primary" fullWidth>
-        xs: 12cols, sm: 6cols
+        xs:12, sm:6
       </Button>
     </Grid>
     <Grid item xs={12} sm={6} uxpId="1.2">
       <Button uxpId="1.2.1" size="medium" variant="outlined" color="primary" fullWidth>
-        xs: 12cols, sm: 6cols
+        xs:12, sm:6
       </Button>
     </Grid>
   </Grid>
