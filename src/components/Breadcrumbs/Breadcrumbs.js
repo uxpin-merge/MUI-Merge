c6e62f5900858a2e90afe99375ddae6df13af45b
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-11-17 16:50:26 -0700

    cleanup

--- src/components/Breadcrumbs/Breadcrumbs.js
+++ src/components/Breadcrumbs/Breadcrumbs.js
@@ -13,26 +13,11 @@ Breadcrumbs.propTypes = {
   /**
    * The content of the component.
    */
-  /** @uxpinignoreprop */
   children: PropTypes.node,
-
   /**
-   * Override or extend the styles applied to the component. See CSS API https://mui.com/api/breadcrumbs/#css for more details.
-   */
-  /** @uxpinignoreprop */
-  classes: PropTypes.object,
-
-  /**
-   * The component used for the root node. Either a string to use a HTML element or a component.
-   */
-  /** @uxpinignoreprop */
-  component: PropTypes.node,
-
-  /**
-   * Override the default label for the expand button.
-   * For localization purposes, you can use the provided translations https://mui.com/guides/localization/.
-   */
-  /** @uxpinignoreprop */
+    * Override the default label for the expand button.
+    * For localization purposes, you can use the provided translations https://mui.com/guides/localization/.
+    */
   expandText: PropTypes.string,
 
   /**
@@ -53,8 +38,18 @@ Breadcrumbs.propTypes = {
   maxItems: PropTypes.number,
 
   /** Custom separator node */
-  /** @uxpinignoreprop */
   separator: PropTypes.node,
+  /**
+   * Override or extend the styles applied to the component. See CSS API https://mui.com/api/breadcrumbs/#css for more details.
+   */
+  classes: PropTypes.object,
+
+  /**
+   * The component used for the root node. Either a string to use a HTML element or a component.
+   */
+  component: PropTypes.node,
+
+
 
   /** The system prop that allows defining system overrides as well as additional CSS styles.
    * See the `sx` page for more details. https://mui.com/system/the-sx-prop/ */
--- src/components/Breadcrumbs/presets/0-default.jsx
+++ src/components/Breadcrumbs/presets/0-default.jsx
@@ -5,10 +5,10 @@ import Link from '../../Link/Link';
 
 export default (
   <Breadcrumbs aria-label="breadcrumb" uxpId="breadcrumb-1">
-    <Link uxpId="link-1" underline="hover" color="inherit" href="/">
+    <Link uxpId="link-1" underline="hover" color="inherit" href="#">
       Files
     </Link>
-    <Link uxpId="link-2" underline="hover" color="inherit" href="/getting-started/installation/">
+    <Link uxpId="link-2" underline="hover" color="inherit" href="#">
       Documents
     </Link>
     <Typography uxpId="link-3" color="text.primary">
