6147da9fae82a20e0425ea2bd40f7662ae79a588
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-11-21 18:16:00 -0700

    cleanup

--- src/components/Button/presets/0-default.jsx
+++ src/components/Button/presets/0-default.jsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import Button from '../Button';
-import Icon from '../../Icon/Icon';
 
 export default (
-  <Button uxpId="Button-1" color="primary" size="medium" variant="contained" startIcon={<Icon uxpId="button-icon">delete</Icon>}>
+  <Button uxpId="Button-1" color="primary" size="medium" variant="contained">
     Button
   </Button>
 );
