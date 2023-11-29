8c2e861c251024599883754a5cefafd4bdb9bb6f
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-11-28 17:14:22 -0700

    cleanup

--- src/components/Hidden/presets/0-default.jsx
+++ src/components/Hidden/presets/0-default.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
 import Hidden from '../Hidden';
+import Box from '../../Box/Box';
 
-export default <Hidden uxpId="hidden-1">Drag components into this wrapper</Hidden>;
+export default <Hidden only="xs" uxpId="hidden-1"><Box textAlign="center" bgcolor="grey.100" uxpId="hidden-box-1">I'm hidden in an xs breakpoint!</Box></Hidden>;
