f1255a78f1cb12425a0e34db1aed05eda61a0e96
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-12-04 11:46:36 -0700

    cleanup

--- src/components/ButtonBase/presets/0-default.jsx
+++ src/components/ButtonBase/presets/0-default.jsx
@@ -1,8 +1,23 @@
 import * as React from 'react';
 import ButtonBase from '../ButtonBase';
+import Avatar from '../../Avatar/Avatar';
+import Typography from '../../Typography/Typography';
 
 export default (
-  <ButtonBase uxpId="ButtonBase-1">
-    A wrapper for custom buttons
+  <ButtonBase sx={{ padding: '4px', borderRadius: '8px' }} uxpId="ButtonBase-1">
+    <Avatar
+      sx={{ width: '32px', height: '32px' }}
+      src="https://i.pravatar.cc/150?img=15"
+      uxpId="avatar-1"
+    />
+    <Typography
+      variant="body1"
+      marginLeft="8px"
+      color="inherit"
+      uxpId="typography-1"
+    >
+      John Doe
+    </Typography>
   </ButtonBase>
+
 );
