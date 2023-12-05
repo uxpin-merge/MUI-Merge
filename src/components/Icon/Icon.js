0b941fa1c5cc76ea74d8fadee8e0c9ef67bd3b9e
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-12-04 18:21:28 -0700

    cleanup

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -4,15 +4,29 @@ import IconM from '@mui/material/Icon';
 import { iconVariants } from './icon-variants';
 
 let icons = document.createElement('link');
-icons.setAttribute('href', 'https://fonts.googleapis.com/icon?family=Material+Icons');
+icons.setAttribute('href', 'https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp');
 icons.setAttribute('rel', 'stylesheet');
 document.head.appendChild(icons);
+
+// icons = document.createElement('link');
+// icons.setAttribute('href', 'https://fonts.googleapis.com/icon?family=Material+Icons+Two+Tone');
+// icons.setAttribute('rel', 'stylesheet');
+// document.head.appendChild(icons);
+
+// icons = document.createElement('link');
+// icons.setAttribute('href', 'https://fonts.googleapis.com/icon?family=Material+Icons+Rounded');
+// icons.setAttribute('rel', 'stylesheet');
+// document.head.appendChild(icons);
+
 
 /**
  * @uxpindocurl https://mui.com/api/icon/
  * @uxpindescription Collection of MUI Icons
  */
 function Icon(props) {
+
+
+
   return <IconM {...props}>{props.children}</IconM>;
 }
 
@@ -42,7 +56,10 @@ Icon.propTypes = {
    * The fontSize applied to the icon. Defaults to 24px, but can be configure to inherit font size.
    */
   fontSize: PropTypes.oneOf(['large', 'medium', 'small', 'inherit']),
-
+  /**
+ * The fontSize applied to the icon. Defaults to 24px, but can be configure to inherit font size.
+ */
+  baseClassName: PropTypes.oneOf(['material-icons', 'material-icons-outlined', 'material-icons-two-tone', 'material-icons-round', 'material-icons-sharp']),
   /**
    * The system prop that allows defining system overrides as well as additional CSS styles.
    */
