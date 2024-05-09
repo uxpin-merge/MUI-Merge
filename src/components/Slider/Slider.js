6a9bc0e6d3a75b10255735259ff45b772312267c
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-05-09 10:43:27 -0600

    cleanup

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -33,7 +33,7 @@ Slider.propTypes = {
 
   /**
    * The value of the slider. For ranged sliders, provide an array with two values.
-   * @uxpinbind onChange 0.target.value
+   * @uxpinbind onChange event.target.value
    */
   value: PropTypes.number | PropTypes.array,
 
